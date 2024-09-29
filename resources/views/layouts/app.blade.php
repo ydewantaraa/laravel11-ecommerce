66daa985f337a4508b5be0f14a1a500e4992b9d0
Yudha Dewantara <yudha.dewantaraa@gmail.com>
2024-09-29 23:59:57 +0700

    menambahkan login dan register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -273,7 +273,7 @@
             </a>
 
             <div class="logo">
-                <a href="index.html">
+                <a href="{{ route('home.index') }}">
                     <img src="{{ asset('assets/images/logo.png') }}" alt="Uomo" class="logo__image d-block" />
                 </a>
             </div>
@@ -313,7 +313,7 @@
                 <div class="overflow-hidden">
                     <ul class="navigation__list list-unstyled position-relative">
                         <li class="navigation__item">
-                            <a href="index.html" class="navigation__link">Home</a>
+                            <a href="{{ route('home.index') }}" class="navigation__link">Home</a>
                         </li>
                         <li class="navigation__item">
                             <a href="shop.html" class="navigation__link">Shop</a>
@@ -394,7 +394,7 @@
         <div class="container">
             <div class="header-desk header-desk_type_1">
                 <div class="logo">
-                    <a href="index.html">
+                    <a href="{{ route('home.index') }}">
                         <img src="{{ asset('assets/images/logo.png') }}" alt="Uomo"
                             class="logo__image d-block" />
                     </a>
@@ -403,7 +403,7 @@
                 <nav class="navigation">
                     <ul class="navigation__list list-unstyled d-flex">
                         <li class="navigation__item">
-                            <a href="index.html" class="navigation__link">Home</a>
+                            <a href="{{ route('home.index') }}" class="navigation__link">Home</a>
                         </li>
                         <li class="navigation__item">
                             <a href="shop.html" class="navigation__link">Shop</a>
@@ -472,14 +472,27 @@
                         </div>
                     </div>
 
-                    <div class="header-tools__item hover-container">
-                        <a href="login.html" class="header-tools__item">
-                            <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <use href="#icon_user" />
-                            </svg>
-                        </a>
-                    </div>
+                    @guest
+                        <div class="header-tools__item hover-container">
+                            <a href="{{ route('login') }}" class="header-tools__item">
+                                <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <use href="#icon_user" />
+                                </svg>
+                            </a>
+                        </div>
+                    @else
+                        <div class="header-tools__item hover-container">
+                            <a href="{{ Auth::user()->utype === 'ADM' ? route('admin.index') : route('user.index') }}"
+                                class="header-tools__item">
+                                <span class="pr-6x">{{ Auth::user()->name }}</span>
+                                <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <use href="#icon_user" />
+                                </svg>
+                            </a>
+                        </div>
+                    @endguest
 
                     <a href="wishlist.html" class="header-tools__item">
                         <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
@@ -508,7 +521,7 @@
             <div class="row row-cols-lg-5 row-cols-2">
                 <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
                     <div class="logo">
-                        <a href="index.html">
+                        <a href="{{ route('home.index') }}">
                             <img src="{{ asset('assets/images/logo.png') }}" alt="SurfsideMedia"
                                 class="logo__image d-block" />
                         </a>
@@ -646,7 +659,8 @@
     <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
         <div class="row text-center">
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('home.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_home" />
@@ -656,7 +670,8 @@
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('home.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_hanger" />
@@ -666,7 +681,8 @@
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('home.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <div class="position-relative">
                         <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\USerController;
+use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +13,12 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/account-dashboard', [USerController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth', AuthAdmin::class])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
